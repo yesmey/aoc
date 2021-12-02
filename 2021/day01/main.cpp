be5b342f3840793780e57d467e731b7dbe305989
yesmey <jesper.meyr@gmail.com>
2021-12-02 13:27:31 +0100

    Rewrite day 1 with ranges

--- 2021/day01/main.cpp
+++ 2021/day01/main.cpp
@@ -1,40 +1,44 @@
 #include <common.h>
 
-auto read_input() {
-    std::vector<u32> buffer;
-    std::string line;
-    std::ifstream ifs("input");
-    while (std::getline(ifs, line)) {
-        buffer.push_back(std::stoi(line));
-    }
-    return buffer;
-}
-
-auto first_solution(const std::vector<u32>& input) {
+u32 first_solution(input_range_of<u32> auto&& range) {
+    u32 prev = std::numeric_limits<u32>::max();
     u32 increases = 0;
-    for (int i = 1; i < input.size(); i++) {
-        if (input[i - 1] < input[i]) {
+
+    for (u32 curr : range) {
+        if (prev < curr) {
             increases++;
         }
+        prev = curr;
     }
+
     return increases;
 }
 
-auto second_solution(const std::vector<u32>& input) {
-    std::vector<u32> measurement_sums;
-    for (int i = 0; i < input.size() - 2; i++) {
-        measurement_sums.push_back(input[i] + input[i + 1] + input[i + 2]);
-    }
-    return first_solution(measurement_sums);
+u32 second_solution(input_range_of<u32> auto&& range) {
+    auto sum_next_three = [prev_1 = 0, prev_2 = 0](u32 curr) mutable {
+        const u32 sum = curr + prev_1 + prev_2;
+        prev_2 = prev_1;
+        prev_1 = curr;
+        return sum;
+    };
+
+    return first_solution(range
+        | rviews::transform(sum_next_three)
+        | rviews::drop(2));
 }
 
 int main() {
-    auto input = read_input();
+    {
+        std::ifstream ifs("input");
+        const auto first = first_solution(ranges::istream_view<u32>(ifs));
+        std::cout << "first answer: " << first << std::endl;
+    }
 
-    const auto first = first_solution(input);
-    std::cout << "first answer: " << first << std::endl;
+    {
+        std::ifstream ifs("input");
+        const auto second = second_solution(ranges::istream_view<u32>(ifs));
+        std::cout << "second answer: " << second << std::endl;
+    }
 
-    const auto second = second_solution(input);
-    std::cout << "second answer: " << second << std::endl;
     return 0;
 }
--- 2021/include/common.h
+++ 2021/include/common.h
@@ -1,14 +1,16 @@
 // don't do this at home lol
 
+#include <cstdint>
 #include <string>
 #include <iostream>
 #include <vector>
-#include <cstdint>
+#include <array>
 #include <ranges>
-#include <cassert>
 #include <fstream>
 #include <algorithm>
 #include <optional>
+#include <limits>
+#include <utility>
 
 typedef int8_t i8;
 typedef uint8_t u8;
@@ -24,3 +26,9 @@ typedef uint64_t u64;
 
 namespace ranges = std::ranges;
 namespace rviews = ranges::views;
+
+// source: https://www.reedbeta.com/blog/ranges-compatible-containers/
+template <typename R, typename T>
+concept input_range_of =
+	std::ranges::input_range<R> &&
+	std::convertible_to<std::ranges::range_value_t<R>, T>;
