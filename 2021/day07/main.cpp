e4a4458ac6bdf48a0d6cd87c3e5748776b517ed3
yesmey <jesper.meyr@gmail.com>
2021-12-07 09:35:02 +0100

    Day 7

--- 2021/day07/main.cpp
+++ 2021/day07/main.cpp
@@ -1,6 +1,71 @@
 #include <common.h>
 
+auto parse_input() {
+    std::vector<i64> result;
+
+    std::ifstream ifs("input");
+    std::string line;
+    if (!std::getline(ifs, line))
+        return result;
+
+    auto int_view =
+        line | rviews::split(',')
+             | rviews::transform([](auto&& subrange) {
+                return convert_to_int(std::string_view(subrange.begin(), subrange.end()));
+             });
+
+    ranges::copy(int_view, std::back_inserter(result));
+    return result;
+}
+
+auto first_solution(const std::vector<i64>& input) {
+    i64 max = *std::max_element(input.begin(), input.end());
+
+    i64 min_fuel = std::numeric_limits<i64>::max();
+    for (i64 i = 1; i < max; i++) {
+        i64 fuel = 0;
+        for (i64 num : input) {
+            fuel += std::abs(num - i);
+        }
+
+        min_fuel = std::min(min_fuel, fuel);
+    }
+
+    return min_fuel;
+}
+
+auto second_solution(const std::vector<i64>& input) {
+    i64 max = *std::max_element(input.begin(), input.end());
+
+    i64 min_fuel = std::numeric_limits<i64>::max();
+    for (i64 i = 1; i < max; i++) {
+        i64 fuel = 0;
+        for (i64 num : input) {
+            auto distance = std::abs(num - i);
+            for (i64 j = 1; j < distance; j++) {
+                fuel += j;
+            }
+            fuel += distance;
+        }
+
+        min_fuel = std::min(min_fuel, fuel);
+    }
+
+    return min_fuel;
+}
+
 int main() {
-    
+    auto input = parse_input();
+
+    {
+        const auto first = first_solution(input);
+        std::cout << "first answer: " << first << std::endl;
+    }
+
+    {
+        const auto second = second_solution(input);
+        std::cout << "second answer: " << second << std::endl;
+    }
+
     return 0;
 }
